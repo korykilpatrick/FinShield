bd918faa2d0b1de337121a09101f811b63a82ef7
korykilpatrick <koryrkilpatrick@gmail.com>
2025-02-03 21:57:08 -0600

    viewing vids and sorta scrolling

--- FinShield/Views/VideoCellView.swift
+++ FinShield/Views/VideoCellView.swift
@@ -4,36 +4,41 @@ import AVKit
 struct VideoCellView: View {
     let video: Video
     @State private var player: AVPlayer? = nil
+    @State private var playerError: Error? = nil
     
     var body: some View {
-        ZStack(alignment: .bottom) {
+        ZStack {
             if let player = player {
                 VideoPlayer(player: player)
-                    .onAppear { player.play() }
-                    .onDisappear { player.pause() }
-                    .ignoresSafeArea()
+                    .onAppear {
+                        player.play()
+                    }
+                    .onDisappear {
+                        player.pause()
+                    }
             } else {
                 Color.black
             }
-            // Overlay caption and link to comments
-            VStack {
-                Spacer()
-                HStack {
-                    Text(video.caption)
-                        .foregroundColor(.white)
-                        .padding()
-                    Spacer()
-                    NavigationLink(destination: CommentsView(videoID: video.id)) {
-                        Image(systemName: "message")
-                            .foregroundColor(.white)
-                            .padding()
-                    }
-                }
+            // Show error if one exists
+            if let error = playerError {
+                Text("Error: \(error.localizedDescription)")
+                    .foregroundColor(.red)
+                    .padding()
             }
         }
         .onAppear {
             if player == nil {
-                player = AVPlayer(url: video.videoURL)
+                let item = AVPlayerItem(url: video.videoURL)
+                // Observe for playback errors
+                NotificationCenter.default.addObserver(forName: .AVPlayerItemFailedToPlayToEndTime,
+                                                       object: item,
+                                                       queue: .main) { notification in
+                    if let error = notification.userInfo?[AVPlayerItemFailedToPlayToEndTimeErrorKey] as? Error {
+                        playerError = error
+                        print("AVPlayer error: \(error.localizedDescription)")
+                    }
+                }
+                player = AVPlayer(playerItem: item)
             }
         }
     }
--- FinShield/Views/VideoFeedView.swift
+++ FinShield/Views/VideoFeedView.swift
@@ -1,40 +1,45 @@
 import SwiftUI
 
-
 struct VideoFeedView: View {
     @StateObject var viewModel = VideoFeedViewModel()
     
     var body: some View {
-        VStack {
+        GeometryReader { geo in
             if viewModel.videos.isEmpty {
-                Text("Click here to watch Jordan MeatSpin!!!")
-                    .foregroundColor(.white)
-                    .padding()
-                    .background(Color.red)
+                // Fallback UI when there are no videos.
+                VStack {
+                    Text("No videos available")
+                        .foregroundColor(.white)
+                        .padding()
+                        .background(Color.red)
+                        .cornerRadius(8)
+                }
+                .frame(width: geo.size.width, height: geo.size.height)
+                .background(Color.black)
             } else {
-                // Your existing TabView code…
+                // Use a rotated TabView to mimic TikTok-style vertical paging.
+                TabView {
+                    ForEach(viewModel.videos) { video in
+                        VideoCellView(video: video)
+                            .frame(width: geo.size.width, height: geo.size.height)
+                            .clipped()
+                            // Rotate the video view 90° so that the TabView can be rotated back.
+                            .rotationEffect(.degrees(90))
+                            .frame(width: geo.size.width, height: geo.size.height)
+                    }
+                }
+                .frame(width: geo.size.width, height: geo.size.height)
+                // Rotate the TabView to allow vertical swiping.
+                .rotationEffect(.degrees(-90))
+                .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
             }
         }
+        .ignoresSafeArea() // Use full screen.
     }
 }
 
-// struct VideoFeedView: View {
-//     @StateObject var viewModel = VideoFeedViewModel()
-    
-//     var body: some View {
-//         GeometryReader { geo in
-//             TabView {
-//                 ForEach(viewModel.videos) { video in
-//                     VideoCellView(video: video)
-//                         .frame(width: geo.size.width, height: geo.size.height)
-//                         .clipped()
-//                         .rotationEffect(.degrees(90))
-//                         .frame(width: geo.size.width, height: geo.size.height)
-//                 }
-//             }
-//             .frame(width: geo.size.width, height: geo.size.height)
-//             .rotationEffect(.degrees(-90))
-//             .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
-//         }
-//     }
-// }
+struct VideoFeedView_Previews: PreviewProvider {
+    static var previews: some View {
+        VideoFeedView()
+    }
+}
