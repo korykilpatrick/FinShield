2c0d56a41103f1e1ca3972815d78200fbc7cab0d
korykilpatrick <koryrkilpatrick@gmail.com>
2025-02-05 16:04:23 -0600

    updated video with new metadata

--- FinShield/Models/Video.swift
+++ FinShield/Models/Video.swift
@@ -1,20 +1,32 @@
 import Foundation
+import FirebaseFirestore
 
 struct Video: Identifiable, Equatable {
     let id: String
+    let videoName: String
+    let videoTitle: String
     let videoURL: URL
     let caption: String
-    
+    let username: String
+    let timestamp: Date
+
     init?(from dict: [String: Any], id: String) {
         guard let urlString = dict["videoURL"] as? String,
-              // Only accept URLs that refer to the HLS master manifest
               urlString.contains("master.m3u8"),
               let url = URL(string: urlString),
-              let caption = dict["caption"] as? String else {
-            return nil
-        }
+              let caption = dict["caption"] as? String,
+              let videoName = dict["videoName"] as? String,
+              let videoTitle = dict["videoTitle"] as? String,
+              let username = dict["username"] as? String,
+              let ts = dict["timestamp"] as? Timestamp
+        else { return nil }
+        
         self.id = id
+        self.videoName = videoName
+        self.videoTitle = videoTitle
         self.videoURL = url
         self.caption = caption
+        self.username = username
+        self.timestamp = ts.dateValue()
     }
 }
